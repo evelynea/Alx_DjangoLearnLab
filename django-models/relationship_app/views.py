909f8dbbae6cc5f2467cf45739f8344bf7581086
Evelyne <evelyneujeneza@gmail.com>
2024-08-17 23:23:25 +0200

    views and URL configuration

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -8,7 +8,7 @@ from .models import Library
 
 def list_books(request):
     books = Book.objects.all()
-    return render(request, 'list_books.html', {'books': books})
+    return render(request, 'relationship_app/list_books.html', {'books': books})
 
 class LibraryDetailView(DetailView):
     model = Library
