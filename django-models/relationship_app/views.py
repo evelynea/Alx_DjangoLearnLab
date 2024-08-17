d26e0c2c1eb61ce12c36c7ed849ca078a44a196a
Evelyne <evelyneujeneza@gmail.com>
2024-08-17 23:38:19 +0200

    views and URL configuration

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 from .models import Book
-from django.views.generic import DetailView
+from django.views.generic.detail import DetailView
 from .models import Library
 
 
