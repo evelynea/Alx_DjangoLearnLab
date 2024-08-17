2d25c783b825990fd293db918060d5647d26e335
Evelyne <evelyneujeneza@gmail.com>
2024-08-17 21:23:46 +0200

    django-models

---
+++ django-models/LibraryProject/bookshelf/create.md
@@ -0,0 +1,11 @@
+# Creating a Book Instance
+
+from bookshelf.models import Book
+book = Book.objects.create(title = "1984", author = "George Orwell", publication_year = 1949)
+print(book)
+
+# expected output
+
+1984
+
+
---
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -0,0 +1,30 @@
+from relationship_app.models import Author, Book, Library, Librarian
+
+def query_books_by_author(author_id):
+    """Query all books by a specific author."""
+    try:
+        author = Author.objects.get(id=author_id)
+        books = Book.objects.filter(author=author)
+        return books
+    except Author.DoesNotExist:
+        return None
+
+def list_books_in_library(library_id):
+    """List all books in a library."""
+    try:
+        library = Library.objects.get(id=library_id)
+        books = library.book_set.all()  # Assuming a ForeignKey relationship between Book and Library
+        return books
+    except Library.DoesNotExist:
+        return None
+
+def retrieve_librarian_for_library(library_id):
+    """Retrieve the librarian for a library."""
+    try:
+        library = Library.objects.get(id=library_id)
+        librarian = Librarian.objects.get(library=library)
+        return librarian
+    except (Library.DoesNotExist, Librarian.DoesNotExist):
+        return None
+
+
