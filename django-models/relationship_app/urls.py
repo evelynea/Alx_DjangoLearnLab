8aeacab61acb8d127dca8bec17c8295c96e743d5
Evelyne <evelyneujeneza@gmail.com>
2024-08-18 23:22:40 +0200

    custom permissions

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-from .views import list_books, LibraryDetailView, admin_view
+from .views import list_books, LibraryDetailView, admin_view, add_book_view, edit_book_view, delete_book_view
 from django.contrib.auth.views import LoginView, LogoutView
 
 
@@ -14,5 +14,8 @@ urlpatterns = [
     path('admin/', admin_view, name='admin_view'),
     path('librarian/', views.librarian_view, name='librarian_view'),
     path('member/', views.member_view, name='member_view'),
+    path('book/add/', add_book_view, name='add_book'),
+    path('book/edit/<int:book_id>/', edit_book_view, name='edit_book'),
+    path('book/delete/<int:book_id>/', delete_book_view, name='delete_book'),
 
 ]
--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,14 +1,15 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 
 # Create your views here.
 from .models import Book
 from django.views.generic.detail import DetailView
-from .models import Library
+from .models import Library, Book
+from .forms import BookForm
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 from django.contrib.auth import login
-from django.contrib.auth.decorators import user_passes_test
+from django.contrib.auth.decorators import user_passes_test, permission_required
 
 
 def list_books(request):
@@ -58,3 +59,39 @@ def Librarian_view(request):
 @user_passes_test(member_check)
 def Member_view(request):
     return render(request, 'relationship_app/member_view.html')
+
+
+
+#custom permissions
+
+
+@permission_required('relationship_app.can_add_book')
+def add_book_view(request):
+    if request.method == 'POST':
+        form = BookForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('book_list')
+    else:
+        form = BookForm()
+    return render(request, 'add_book.html', {'form': form})
+
+@permission_required('relationship_app.can_change_book')
+def edit_book_view(request, book_id):
+    book = get_object_or_404(Book, pk=book_id)
+    if request.method == 'POST':
+        form = BookForm(request.POST, instance=book)
+        if form.is_valid():
+            form.save()
+            return redirect('book_list')
+    else:
+        form = BookForm(instance=book)
+    return render(request, 'edit_book.html', {'form': form})
+
+@permission_required('relationship_app.can_delete_book')
+def delete_book_view(request, book_id):
+    book = get_object_or_404(Book, pk=book_id)
+    if request.method == 'POST':
+        book.delete()
+        return redirect('book_list')
+    return render(request, 'delete_book.html', {'book': book})
