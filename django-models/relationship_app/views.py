c4be69e6010857fed7e063a80cb1fd53f64357da
Evelyne <evelyneujeneza@gmail.com>
2024-08-18 12:30:28 +0200

    user authentication

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -22,7 +22,7 @@ class LibraryDetailView(DetailView):
 class RegisterView(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('login')
-    template_name = 'register.html'
+    template_name = 'relationship_app/register.html'
 
 def register(request):
     if request.method == 'POST':
