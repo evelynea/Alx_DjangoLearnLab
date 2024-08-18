5c3bd909fe2e9aa85e5208232cb6d76ad4b522e7
Evelyne <evelyneujeneza@gmail.com>
2024-08-18 12:00:25 +0200

    user authentication

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -4,6 +4,9 @@ from django.shortcuts import render
 from .models import Book
 from django.views.generic.detail import DetailView
 from .models import Library
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
 
 
 def list_books(request):
@@ -14,3 +17,8 @@ class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
+
+class RegisterView(CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'register.html'
