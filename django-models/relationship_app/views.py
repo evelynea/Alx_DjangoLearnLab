5cc7a80e6abf483fd3445dc09d872b10b4a73904
Evelyne <evelyneujeneza@gmail.com>
2024-08-18 12:02:51 +0200

    user authentication

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -7,6 +7,7 @@ from .models import Library
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
+from django.contrib.auth import login
 
 
 def list_books(request):
