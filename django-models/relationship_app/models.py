4300cadb4c07f14bbf78a41c088b2816542b0d4f
Evelyne <evelyneujeneza@gmail.com>
2024-08-18 13:13:20 +0200

    role-based access

--- django-models/relationship_app/models.py
+++ django-models/relationship_app/models.py
@@ -30,7 +30,7 @@ class UserProfile(models.Model):
     )
 
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    role = models.CharField(max_length=10, choices=ROLE_CHOICES)
+    role = models.TextChoices("Member", "Admin")
 
     def __str__(self):
         return f"{self.user.username} - {self.role}"
