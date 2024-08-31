f76420e5563b6946873875d8e902a113e9010e08
Evelyne <evelyneujeneza@gmail.com>
2024-08-31 02:55:15 +0200

    CRID operations with viewsets and Routers

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,5 +1,9 @@
 from .views import BookList
+from rest_framework import routers
+from .views import BookViewSet
 
+router = routers.DefaultRouter()
+router.register(r'books', BookViewSet, basename='book')
 urlpatterns = [
     path("api/books", BookList.as_view(), name="book_list_create"),
 ]
--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
-from rest_framework import generics
+from rest_framework import generics, viewsets
 from .models import Book
 from .serializers import BookSerializers
 
-class BookList (generics.ListAPIView):
+#class BookList (generics.ListAPIView):
+ #   queryset = Book.objects.all()
+  #  serializer_class = BookSerializers
+
+class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializers
