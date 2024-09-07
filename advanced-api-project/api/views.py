4eee750924471bc3d327dabe99aa2e8b573771ab
Evelyne <evelyneujeneza@gmail.com>
2024-09-07 11:29:46 +0200

    filtering, searching and ordering with DRF

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render
 
 from rest_framework import generics, filters
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
-from django_filters.rest_framework import DjangoFilterBackend
+from django_filters import rest_framework
 from .models import Book
 from .serializers import BookSerializer
 
