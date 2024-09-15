a13c193e1eb40047efcd7c1d7de9c890128442ff
Evelyne <evelyneujeneza@gmail.com>
2024-09-15 21:30:50 +0200

    filtering using tags

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -7,7 +7,11 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ['title', 'content', 'tags']
-    
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'content': forms.Textarea(attrs={'class': 'form-control'}),
+            'tags': TagWidget(attrs={'class': 'form-control', 'placeholder': 'Add tags'}),
+        }
 
 class CommentForm(forms.ModelForm):
     class Meta:
