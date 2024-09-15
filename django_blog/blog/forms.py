a422ad1314087fc323702693b30e732017ce3fb4
Evelyne <evelyneujeneza@gmail.com>
2024-09-15 21:12:20 +0200

    filtering using tags

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -3,10 +3,10 @@ from .models import Post, Comment
 from taggit.forms import TagWidget
 
 class PostForm(forms.ModelForm):
+    widgets = {'tags': TagWidget(attrs={'class': 'form-control', 'placeholder': 'Add tags here'}),}
     class Meta:
         model = Post
         fields = ['title', 'content', 'tags']
-        widgets = {'tags': TagWidget(attrs={'class': 'form-control', 'placeholder': 'Add tags here'}),}
 
 class CommentForm(forms.ModelForm):
     class Meta:
