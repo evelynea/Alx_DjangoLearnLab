d4a96ec016406032f805b8a3b6426ddc3d9ec75d
Evelyne <evelyneujeneza@gmail.com>
2024-09-21 16:41:54 +0200

    social media api

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -7,8 +7,8 @@ from rest_framework.authtoken.models import Token
 User = get_user_model()
 
 class UserRegistrationSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True, required=True, style={'input_type': 'password'})
-    password2 = serializers.CharField(write_only=True, required=True, label='Confirm Password', style={'input_type': 'password'})
+    password = serializers.CharField()
+    password2 = serializers.CharField()
 
     bio = serializers.CharField(required=False, allow_blank=True)
     profile_picture = serializers.ImageField(required=False, allow_null=True)
@@ -25,23 +25,11 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         validated_data.pop('password2')  # Remove password2 as it's not needed
         user = User.objects.create_user(**validated_data)
+        Token.objects.create(user=user)
         return user
 
 class UserLoginSerializer(serializers.Serializer):
     username = serializers.CharField(required=True)
     password = serializers.CharField(write_only=True, required=True, style={'input_type': 'password'})
 
-
-    def validate(self, attrs):
-        username = attrs.get('username')
-        password = attrs.get('password')
-
-        if username and password:
-            user = authenticate(username=username, password=password)
-            if not user:
-                raise serializers.ValidationError("Invalid credentials.")
-        else:
-            raise serializers.ValidationError("Both username and password are required.")
-
-        attrs['user'] = user
-        return attrs
+    
