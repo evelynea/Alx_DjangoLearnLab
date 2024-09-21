af7c3986f390b3676cc1a473d7d132837e41ac6f
Evelyne <evelyneujeneza@gmail.com>
2024-09-21 16:43:50 +0200

    social media api

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,21 +1,34 @@
 from rest_framework import serializers
-from django.contrib.auth import authenticate
-from .models import User
+from django.contrib.auth import get_user_model, authenticate
 from rest_framework.authtoken.models import Token
-
 
 User = get_user_model()
 
 class UserRegistrationSerializer(serializers.ModelSerializer):
-    password = serializers.CharField()
-    password2 = serializers.CharField()
-
+    password = serializers.CharField(
+        write_only=True, 
+        required=True, 
+        style={'input_type': 'password'}
+    )
+    password2 = serializers.CharField(
+        write_only=True, 
+        required=True, 
+        label='Confirm Password', 
+        style={'input_type': 'password'}
+    )
     bio = serializers.CharField(required=False, allow_blank=True)
     profile_picture = serializers.ImageField(required=False, allow_null=True)
 
     class Meta:
         model = User
-        fields = ('username', 'email', 'password', 'password2', 'bio', 'profile_picture')
+        fields = (
+            'username', 
+            'email', 
+            'password', 
+            'password2', 
+            'bio', 
+            'profile_picture'
+        )
 
     def validate(self, attrs):
         if attrs['password'] != attrs['password2']:
@@ -25,11 +38,30 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         validated_data.pop('password2')  # Remove password2 as it's not needed
         user = User.objects.create_user(**validated_data)
-        Token.objects.create(user=user)
+        Token.objects.create(user=user)  # Create a token for the new user
         return user
 
 class UserLoginSerializer(serializers.Serializer):
-    username = serializers.CharField(required=True)
-    password = serializers.CharField(write_only=True, required=True, style={'input_type': 'password'})
+    username = serializers.CharField(
+        required=True, 
+        max_length=150
+    )
+    password = serializers.CharField(
+        write_only=True, 
+        required=True, 
+        style={'input_type': 'password'}
+    )
 
-    
+    def validate(self, attrs):
+        username = attrs.get('username')
+        password = attrs.get('password')
+
+        if username and password:
+            user = authenticate(username=username, password=password)
+            if not user:
+                raise serializers.ValidationError("Invalid credentials.")
+        else:
+            raise serializers.ValidationError("Both username and password are required.")
+
+        attrs['user'] = user
+        return attrs
